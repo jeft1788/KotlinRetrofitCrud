753040bded68b84b2d241a734f504a657ed18c13
jeft1788 <jeft1788@gmail.com>
2023-11-06 22:08:25 -0500

    arreglo update

--- app/src/main/java/ir/almasapps/kotlinretrofitcrud/UpdateActivity.kt
+++ app/src/main/java/ir/almasapps/kotlinretrofitcrud/UpdateActivity.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -19,14 +20,12 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class UpdateActivity : AppCompatActivity() {
+class UpdateActivity : AppCompatActivity(), View.OnClickListener  {
     lateinit var progressDialog: ProgressDialog
     private lateinit var strId: String
     private lateinit var strColor:String
 
     lateinit var note: Note
-    lateinit var update_txtNote: EditText
-    lateinit var update_txtTitle: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +36,26 @@ class UpdateActivity : AppCompatActivity() {
         progressDialog = ProgressDialog(this)
         val update_btnUpdate = findViewById<Button>(R.id.update_btnUpdate)
         val update_btnDelete = findViewById<Button>(R.id.update_btnDelete)
-
+        val img1 = findViewById<ImageView>(R.id.img1)
+        val img2 = findViewById<ImageView>(R.id.img2)
+        val img3 = findViewById<ImageView>(R.id.img3)
+        val img4 = findViewById<ImageView>(R.id.img4)
+        val img5 = findViewById<ImageView>(R.id.img5)
+        val img6 = findViewById<ImageView>(R.id.img6)
+        val img7 = findViewById<ImageView>(R.id.img7)
+        val img8 = findViewById<ImageView>(R.id.img8)
+        val img9 = findViewById<ImageView>(R.id.img9)
+        val img10 = findViewById<ImageView>(R.id.img10)
+        img1.setOnClickListener(this)
+        img2.setOnClickListener(this)
+        img3.setOnClickListener(this)
+        img4.setOnClickListener(this)
+        img5.setOnClickListener(this)
+        img6.setOnClickListener(this)
+        img7.setOnClickListener(this)
+        img8.setOnClickListener(this)
+        img9.setOnClickListener(this)
+        img10.setOnClickListener(this)
         var note = Note(
             intent.getStringExtra("itemId").toString(),
             intent.getStringExtra("itemTitle").toString(),
@@ -115,10 +133,27 @@ class UpdateActivity : AppCompatActivity() {
     }
 
     fun getColor(view: View) {
+        val update_txtNote = findViewById<EditText>(R.id.update_txtNote)
+        val update_txtTitle = findViewById<EditText>(R.id.update_txtTitle)
         val viewColor = view.background as ColorDrawable
         val colorId = viewColor.color
         strColor = String.format("#%06X", 0xFFFFFF and colorId)
         update_txtTitle.setBackgroundColor(colorId)
         update_txtNote.setBackgroundColor(colorId)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.img1-> getColor(v)
+            R.id.img2-> getColor(v)
+            R.id.img3-> getColor(v)
+            R.id.img4-> getColor(v)
+            R.id.img5-> getColor(v)
+            R.id.img6-> getColor(v)
+            R.id.img7-> getColor(v)
+            R.id.img8-> getColor(v)
+            R.id.img9-> getColor(v)
+            R.id.img10-> getColor(v)
+        }
     }
 }
